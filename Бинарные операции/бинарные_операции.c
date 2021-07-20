f9d18d4f94347ddec31fc6177c59be42047feeef
KotFedot21 <86765128+KotFedot21@users.noreply.github.com>
2021-07-20 11:13:21 +0300

    Add files via upload

---
+++ Бинарные операции/бинарные_операции.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 0x00DDAABB;
+    unsigned char c;
+    c = (a& 0xFF);
+    printf ( "First byte of a =%x\n",c);
+    c = ((a>>8)& 0xFF);
+    printf ( "First byte of a =%x\n",c);
+    c = ((a>>16)& 0xFF);
+    printf ( "First byte of a =%x\n",c);
+
+    a = a & 0x00FFFFFF;
+    printf("\nChanged number is %x\n ", a );
+
+    return 0;
+}

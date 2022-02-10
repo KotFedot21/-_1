02540efc2372236ef5c3ac9781e52079d110e3aa
KotFedot21 <86765128+KotFedot21@users.noreply.github.com>
2022-02-10 13:28:52 +0700

    Update редми.py

--- элтекс/редми.py
+++ элтекс/редми.py
@@ -4,13 +4,16 @@ from datetime import date
 from redminelib import Redmine  
 from datetime import datetime  
 from datetime import timedelta  
-time = datetime.now()  
+##time = datetime.now()  
+
+time = datetime.date(2022, 1, 12)
+
 ##checktime = time -timedelta(minutes = 5) 
 
 
 
-URL=' '
-KEY=' '
+URL='http://red.eltex.loc'
+KEY='08c017a3b50e09357a72e4448cc1aa6f09271755'
 redmine = Redmine(URL, key=KEY)
 userlist = [
         69
@@ -19,7 +22,7 @@ userlist = [
 statuslist = [
         1
     ]
-def GetAllIssuesInAllProjects(redmine, userlist, statuslist):
+def GetAllIssuesInAllProjects(redmine, userlist, statuslist, time):
     for userid in userlist:
         try:
             print(redmine.user.get(userid).firstname, redmine.user.get(userid).lastname)
@@ -27,9 +30,9 @@ def GetAllIssuesInAllProjects(redmine, userlist, statuslist):
             prjc = 0
             for msp in ms.memberships:
                 prjc += 1
-                print('\tPROJECT NAME:', msp.project.name, 'ID:', msp.project.id)
+               ## print('\tPROJECT NAME:', msp.project.name, 'ID:', msp.project.id)
                 for statusid in statuslist:
-                    issues = redmine.issue.filter(project_id = msp.project.id, status_id = statusid, created_on = '>=%s'%checktime.strftime('%Y-%m-%d'),cf_2=userid)
+                    issues = redmine.issue.filter(project_id = msp.project.id, status_id = statusid, created_on = '>=%s'%time.strftime('%Y-%m-%d'),cf_2=userid)
                     for issue in issues:
                         print('\t\tНомер:', issue.id, 'Статус:', issue.status.name, 'Версия:', issue.fixed_version.name, 'Тема:', issue.subject)
                 print('Количество проектов:', prjc)

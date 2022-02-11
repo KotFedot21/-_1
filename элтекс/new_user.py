97ebfa4a81d7b17a055ed627b57e326e13f8722b
KotFedot21 <86765128+KotFedot21@users.noreply.github.com>
2022-02-11 16:08:40 +0700

    Create new_user.py

---
+++ элтекс/new_user.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+sg.theme('Reddit') 
+userID = [ 69, 12, 123]
+layout = [
+    [sg.Text('Please enter your ID')],
+    [sg.Text('ID', size=(15, 1)), sg.InputText()],
+    [sg.OK()]
+    ]
+window = sg.Window('New User', layout)
+event, values = window.read()
+window.close()
+
+userID.append(values[0])
+print (userID)

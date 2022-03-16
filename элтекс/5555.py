b2b0236c690ff7a72147224fc071657478945db5
KotFedot21 <86765128+KotFedot21@users.noreply.github.com>
2022-03-16 21:06:56 +0700

    Create 5555.py

---
+++ элтекс/5555.py
@@ -0,0 +1,12 @@
+qwe=[
+    "hbfrggrh",
+    "rgsethsh",
+    "rhthrdth",
+    "fdgxdth"
+]
+headings = []
+
+for irr in qwe:
+      headings.append(irr)
+#  headings = ['User', "Название проекта ", 'номер задачи', 'Статус']
+  print(headings)
